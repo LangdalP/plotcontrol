a4344240ddc23f694b2e0f7297fe9d0aa2f02404
Peder Voldnes Langdal <pederlangdal@bekk.no>
2018-10-03 19:46:04 +0200

    Siste stuff

--- experiments/joystick_plotcontrol.py
+++ experiments/joystick_plotcontrol.py
@@ -4,6 +4,7 @@ from pygame.joystick import *
 from utils.io_utils import *
 from utils.colors import *
 from utils.gfx_utils import *
+from utils.plotter_utils import *
 import utils.svg_utils as svg
 
 import sys
@@ -12,23 +13,32 @@ import math
 import time
 
 # Constants
-JOYSTICK_POLL_INTERVAL = 0.03 # Seconds
-JOYSTICK_SPEED_MULTIPLIER = 3
+JOYSTICK_POLL_INTERVAL = 0.00 # Seconds
+JOYSTICK_SPEED_MULTIPLIER = 0.7
 
-POINTER_START_X = 50
-POINTER_START_Y = 50
+POINTER_START_X = 0
+POINTER_START_Y = 0
+
+PLOTTER_X_MIN = 1
+PLOTTER_Y_MIN = 1
+PLOTTER_X_MAX = 25
+PLOTTER_Y_MAX = 17
 
 # Global state
 pointer_x = POINTER_START_X
 pointer_y = POINTER_START_Y
 relative_line_segments = []
 
+plotter_x = 0
+plotter_y = 0
+
+
 def xy_filtered(x, y):
     radius = math.sqrt(x**2 + y**2)
     return (0, 0) if radius < 0.2 else (x, y)
 
-def start_game_loop(surface, joystick):
-    global pointer_x, pointer_y, relative_line_segments
+def start_game_loop(surface, joystick, plotter):
+    global pointer_x, pointer_y, relative_line_segments, plotter_x, plotter_y
     pygame.display.update()
 
     lastJoystickPollTime = time.time()
@@ -37,25 +47,28 @@ def start_game_loop(surface, joystick):
         for event in new_events:
             if event.type == pygame.JOYBUTTONDOWN:
                 print("Joystick button pressed.")
+                plotter.pendown()
             if event.type == pygame.JOYBUTTONUP:
                 print("Joystick button released.")
-                path = svg.create_path(POINTER_START_X, POINTER_START_Y, relative_line_segments)
-                svg.save_and_open_svg(path)
+                plotter.penup()
             if event.type == QUIT:
+                plotter.moveto(0, 0)
+                plotter.disconnect()
                 cleanup_and_exit()
         
         currentTime = time.time()
         timeSinceLastPoll = currentTime - lastJoystickPollTime
         if timeSinceLastPoll > JOYSTICK_POLL_INTERVAL:
+            lastJoystickPollTime = time.time()
             x_raw = joystick.get_axis(0)
             y_raw = joystick.get_axis(1)
             (x, y) = xy_filtered(x_raw, y_raw)
             (dx, dy) = (JOYSTICK_SPEED_MULTIPLIER*x, JOYSTICK_SPEED_MULTIPLIER*y)
-            pointer_x = pointer_x + dx
-            pointer_y = pointer_y + dy
-            draw_pointer(surface, pointer_x, pointer_y)
-            pygame.display.update()
-            lastJoystickPollTime = time.time()
+            if not (dx == 0 and dy == 0):
+                plotter_x = min(max(plotter_x + dx, PLOTTER_X_MIN), PLOTTER_X_MAX)
+                plotter_y = min(max(plotter_y + dy, PLOTTER_Y_MIN), PLOTTER_Y_MAX)
+                plotter.goto(plotter_x, plotter_y)
+                print(f'Plot moveto: {plotter_x}, {plotter_y}')
             if dx != 0 and dy != 0:
                 relative_line_segments.append((dx, dy))
 
@@ -66,8 +79,12 @@ def main():
     joystick = init_joystick()
     if joystick == None:
         cleanup_and_exit()
+
+    plotter = init_plotter_interactive()
+    if plotter == None:
+        cleanup_and_exit()
     
-    start_game_loop(surface, joystick)
+    start_game_loop(surface, joystick, plotter)
     
 if __name__ == '__main__':
     main()
--- experiments/utils/svg_utils.py
+++ experiments/utils/svg_utils.py
@@ -8,8 +8,8 @@ def create_path(start_x, start_y, relative_lines):
     relative_lines_string = ' '.join(relative_line_commands)
     full_path = f'M {start_x} {start_y} {relative_lines_string}'
     path = parse_path(full_path)
-    smoothened = smoothed_path(path)
-    return smoothened
+    # smoothened = smoothed_path(path)
+    return path
 
 def save_and_open_svg(path):
     disvg(path)
